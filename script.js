036c192f751e7f18751d86b90af0a4403b3cc768
riachkyle <riachkyle@gmail.com>
2014-07-25 16:42:40 -0700

    right before firebase deployment

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-var gameApp = angular.module('gameApp',[]);
+var gameApp = angular.module('gameApp',["firebase"]);
  
 
-gameApp.controller("ticController", function ($scope){
+gameApp.controller("ticController", function ($scope, $firebase){
 
 //variables ==================================
+var TTTRef = new Firebase("https://tmnt-ttt.firebaseIO.com") ;
+
+$scope.remoteGameContainer = 
+    $firebase(new Firebase("https://tmnt-ttt.firebaseIO.com" + '/remoteGameContainer')) ;
+    
 var board
 $scope.board = [" ", " "," "," "," "," "," "," "," "];
 $scope.clicked = 0;
 $scope.loader = 0;
 $scope.charaimage = "images/leo.png",
 $scope.charnum = 0;
-// var TTTRef = new Firebase("https://tttbyrichard.firebaseIO.com")
 
-// var i = 0;
-// var k = 0;
-// var rl = 3;
+$scope.gameContainer = {
+    cellListArray: $scope.board,    
+  } ;
+
+    $scope.remoteGameContainer.$bind($scope, "gameContainer");
+
+     $scope.$watch('gameContainer', function() {
+    console.log('gameCountainer changed!') ;
+  }) ;
  
 // turn counter and image placer
 $scope.clicker = function(b) {
@@ -34,7 +44,7 @@ if ($scope.board[b] == " ") {
       $scope.checkrow();
       $scope.checkcol();
       $scope.checkdiag();
-      console.log('1st ok');    
+         
     }
   }
   };
@@ -52,7 +62,6 @@ if ($scope.board[b] == " ") {
     }; 
   };
 
-
   $scope.checkcol = function(){
     var rl = 3
     for (i=0; i<rl; ++i) {
@@ -65,7 +74,6 @@ if ($scope.board[b] == " ") {
         }
       }; 
     }; 
-
 
     $scope.checkdiag = function() {
       var rl = 3
@@ -81,22 +89,6 @@ if ($scope.board[b] == " ") {
       };
     };
     };
-
-
-//alternate win solution
-// $scope.wincheckrow = function() {
-//   var rl = 3; 
-//   for (var i=0; i < (rl*rl); i+=rl) {
-//     var k;
-//     for (k=0; k < (rl - 1);) {
-//       if(($scope.board[i + k]!= $scope.board[i + k+1]) || ($scope.board[i + k] == " ")) break;
-//       if(++k == (rl-1))
-//         console.log('All are ok - row ' + (i/3));
-//     }
-//     console.log($scope.board[k])
-//   };
-// //  $scope.checkcol();
-// };
 
 $scope.youwin = function() {
   alert("you win!");
@@ -139,45 +131,179 @@ $scope.leof = function() {
     $scope.charaimage = "images/leo.png";
   }
 };
-//=============================================
 
-
-// $scope.character = [
-//     {
-//       name: 'decaf',
-//       roastLevel: 8,
-//       origin: 'South America',
-//       image: "images/coffee1.jpg",
-//       showPuppy: false,
-//       price: 3,
-//       quantity: 0
-//     },
-//     {
-//       name: 'espresso',
-//       roastLevel: 6,
-//       origin: 'Hawaii',
-//       image: "images/coffee2.jpg",
-//       showPuppy: false,
-//       price: 10,
-//       quantity: 1000
-//     },
-//     {
-//       name: 'double shot of espresso',
-//       roastLevel: 6,
-//       origin: 'Hawaii',
-//       image: "images/coffee3.jpg",
-//       price: 20,
-//       quantity: 24 
-//     },
-//     {
-//       name: 'cappuccino',
-//       roastLevel: 6,
-//       origin: 'Hawaii',
-//       image: "images/coffee3.jpg",
-//       price: 7,
-//       quantity: 0
+//alternate win solution
+// $scope.wincheckrow = function() {
+//   var rl = 3; 
+//   for (var i=0; i < (rl*rl); i+=rl) {
+//     var k;
+//     for (k=0; k < (rl - 1);) {
+//       if(($scope.board[i + k]!= $scope.board[i + k+1]) || ($scope.board[i + k] == " ")) break;
+//       if(++k == (rl-1))
+//         console.log('All are ok - row ' + (i/3));
 //     }
-//   ]
+//     console.log($scope.board[k])
+//   };
+// //  $scope.checkcol();
+// };
+
+}); //end of controller
+
+gameApp.controller("ticController2", function ($scope){
+
+//variables ==================================
+var board
+$scope.board = [" ", " "," "," "," "," "," "," "," "];
+$scope.clicked = 0;
+$scope.loader = 0;
+$scope.charaimage = "images/leo.png",
+$scope.charnum = 0;
+
+$scope.gameContainer = {
+    cellListArray: $scope.board,  
+  } ;
+
+// turn counter and image placer
+$scope.clicker = function(b) {
+if ($scope.board[b] == " ") {
+
+    $scope.clicked++;
+    // console.log($scope.clicked);
+      if ($scope.clicked % 2 == 0) { 
+      $scope.board[b] = 'O';
+      $scope.checkrow();
+      $scope.checkcol();
+      $scope.checkdiag();
+     
+    } 
+    else {
+      $scope.board[b] = 'X';
+      $scope.checkrow();
+      $scope.checkcol();
+      $scope.checkdiag();
+         
+    }
+  }
+  };
+
+  $scope.checkrow = function(){
+    console.log('1st ok')
+    var rl = 3
+    for (i=0; i < (rl*rl); i += rl) {
+        if ($scope.board[i] === $scope.board[i+1] && $scope.board[i] === $scope.board[i+2] && ($scope.board[i] != " ")) {
+          $scope.youwin();
+        }
+        else {
+        }
+    }; 
+  };
 
 
+  $scope.checkcol = function(){
+    var rl = 3
+    for (i=0; i<rl; ++i) {
+      if ($scope.board[i] === $scope.board[i+rl] && $scope.board[i] === $scope.board[i+(2*rl)] && ($scope.board[i] != " ")) {
+          $scope.youwin();
+        } 
+      else {
+        }
+      }; 
+    }; 
+
+    $scope.checkdiag = function() {
+      var rl = 3
+      for (i = 0; i < rl; ++i) {
+        if (i === 0 || i === 2) {
+        if ($scope.board[i] === $scope.board[i+(rl+1)] && $scope.board[i] === $scope.board[(i+2)+(2*rl)] && ($scope.board[i] != " ")) {
+          $scope.youwin();
+         } else if ($scope.board[i] === $scope.board[2*i] && $scope.board[i] === $scope.board[3*i] && (i != 0) && ($scope.board[i] != " ")) {
+          $scope.youwin();
+        } else {
+            console.log('4 ok');
+        }
+      };
+    };
+    };
+
+$scope.youwin = function() {
+  alert("you win!");
+};
+}); //end of controller
+
+gameApp.controller("ticController3", function ($scope){
+
+//variables ==================================
+var board
+$scope.board = [" ", " "," "," "," "," "," "," "," "];
+$scope.clicked = 0;
+$scope.loader = 0;
+$scope.charaimage = "images/leo.png",
+$scope.charnum = 0;
+
+$scope.gameContainer = {
+    cellListArray: $scope.board,  
+  } ;
+
+// turn counter and image placer
+$scope.clicker = function(b) {
+if ($scope.board[b] == " ") {
+
+    $scope.clicked++;
+    // console.log($scope.clicked);
+      if ($scope.clicked % 2 == 0) { 
+      $scope.board[b] = 'O';
+      $scope.checkrow();
+      $scope.checkcol();
+      $scope.checkdiag();
+    } 
+    else {
+      $scope.board[b] = 'X';
+      $scope.checkrow();
+      $scope.checkcol();
+      $scope.checkdiag();   
+    }
+  }
+  };
+
+  $scope.checkrow = function(){
+    console.log('1st ok')
+    var rl = 3
+    for (i=0; i < (rl*rl); i += rl) {
+        if ($scope.board[i] === $scope.board[i+1] && $scope.board[i] === $scope.board[i+2] && ($scope.board[i] != " ")) {
+          $scope.youwin();
+        }
+        else {
+        }
+    }; 
+  };
+
+  $scope.checkcol = function(){
+    var rl = 3
+    for (i=0; i<rl; ++i) {
+      if ($scope.board[i] === $scope.board[i+rl] && $scope.board[i] === $scope.board[i+(2*rl)] && ($scope.board[i] != " ")) {
+          $scope.youwin();
+        } 
+      else {
+        }
+      }; 
+    }; 
+
+    $scope.checkdiag = function() {
+      var rl = 3
+      for (i = 0; i < rl; ++i) {
+        if (i === 0 || i === 2) {
+        if ($scope.board[i] === $scope.board[i+(rl+1)] && $scope.board[i] === $scope.board[(i+2)+(2*rl)] && ($scope.board[i] != " ")) {
+          $scope.youwin();
+         } else if ($scope.board[i] === $scope.board[2*i] && $scope.board[i] === $scope.board[3*i] && (i != 0) && ($scope.board[i] != " ")) {
+          $scope.youwin();
+        } else {
+            console.log('4 ok');
+        }
+      };
+    };
+    };
+
+$scope.youwin = function() {
+  alert("you win!");
+};
 }); //end of controller
